c140705fb2184a4f5e24113d7dd8a171090e6f37
DILSHAN PATHIRAGE <danidutharuka12345@gmail.com>
2025-01-02 11:09:26 +0530

    update

---
+++ suppor/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.suppor.p4_group_8_repo.application.suppor {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.suppor.p4_group_8_repo.application.suppor to javafx.fxml;
+    exports org.suppor.p4_group_8_repo.application.suppor;
+}
---
+++ suppor/src/main/java/org/suppor/p4_group_8_repo/application/suppor/HelloController.java
@@ -0,0 +1,6 @@
+package org.suppor.p4_group_8_repo.application.suppor;
+
+
+public class HelloController {
+
+}
